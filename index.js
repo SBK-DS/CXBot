ddda7a10ffe4aa8866135d4114d6097cb3a4cea6
SBK-DS <syedbasitkhurshid@gmail.com>
2023-06-04 21:27:22 +0500

    code refined

--- index.js
+++ index.js
@@ -24,27 +24,19 @@ app.get('/', (req, res) => {
 
 
 app.post('/', async (req, res) => {
-  console.log('post called')
-  const intentName = req.body.queryResult.intent.displayName;;
 
-  console.log(intentName)
+  const intentName = req.body.queryResult.intent.displayName;;
 
   if (intentName === 'Welcome') {
     const fulfillmentText = "Hi, this is your CXBot. How can I help you?";
-    console.log('Intent Welcome called')
     return res.json({ fulfillmentText });
   }
 
   if (intentName === 'CheckOrderStatus') {
-    console.log('Intent OrderId')
-    const orderID = req.body.queryResult.parameters.orderID;
-    console.log(orderID)
 
-    // Call a function or API to retrieve the order status based on the orderID
-    // console.log('shipment function', await getShipmentDate(orderID))
+    const orderID = req.body.queryResult.parameters.orderID;
     const shipmentDate = await getShipmentDate(orderID)
     const humanReadableDate = moment(shipmentDate).format('dddd, DD MMM YYYY');
-    console.log('shipment date is', shipmentDate)
 
     const fulfillmentText = `Your order ${orderID} will be shipped on ${humanReadableDate}`;
     return res.json({ fulfillmentText });
@@ -60,52 +52,4 @@ app.post('/', async (req, res) => {
 app.listen(5000, (req, res) => {
   console.log('Webhook server is listening on port 5000');
 });
-
-
-// const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const axios = require('axios');
-// const dailogflow = require('dialogflow')
-// const { WebhookClient } = require('dialogflow-fulfillment')
-
-// app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//   console.log('get called')
-//     res.json({ 'Worked': 'It worked' });
-//   });
-
-// app.post('/webhook', express.json(), (req, res) => {
-  
-//   console.log('webhook called')
-//   const _agent = new WebhookClient({request: req, response: res})
-
-//   //welcome
-
-//   function welcome(agent) {
-//     agent.add(`Hi this is your CXBot. How can I help `)
-//   }
-
-//   const intentMap = new Map()
-//   intentMap.set('Default Welcome Intent', demo)
-//   _agent.handleRequest(intentMap)
-
-
-//   //check order
-
-//   function checkOrder(agent) {
-//     agent.add(`What is your orderID`)
-//   }
-
-//   const intentMap = new Map()
-//   intentMap.set('Default Welcome Intent', demo)
-//   _agent.handleRequest(intentMap)
-
-
-// })
-
-// app.listen(5000, (req, res) => {
-//   console.log('Webhook server is listening on port 5000');
-// });
 
